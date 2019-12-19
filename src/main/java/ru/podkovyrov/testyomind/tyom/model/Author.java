be6736b69b37ffef670b7f10de9d46c428a357da
Denis <denis.podkovyrov@gmail.com>
2019-12-19 23:54:15 +0300

    feat: added AuthorService

--- src/main/java/ru/podkovyrov/testyomind/tyom/model/Author.java
+++ src/main/java/ru/podkovyrov/testyomind/tyom/model/Author.java
@@ -1,15 +1,18 @@
 package ru.podkovyrov.testyomind.tyom.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
 import java.util.List;
 
 import static ru.podkovyrov.testyomind.tyom.model.EntityConstrainConstants.MAX_ACCOUNT_NAME;
 
 @Entity
 @Data
+@NoArgsConstructor
 @Table(name = "Author")
 public class Author {
     @Id
@@ -35,4 +38,13 @@ public class Author {
 
     @Column(name = "userpic")
     private String userpic;
+
+    public Author(String name, String pic, String login, String email) {
+        this.name = name;
+        this.userpic = pic;
+        this.login = login;
+        this.yotests = new ArrayList<>();
+        this.status = Status.ACTIVE;
+        this.email = email;
+    }
 }
